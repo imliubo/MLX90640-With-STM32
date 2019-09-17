9a168d98c7e11603ee0c1cc78a406adfce4d398f
IAMLIUBO <imliubo@makingfun.xyz>
2019-09-17 16:46:03 +0800

    mlx90640 with stm32 swi2c frist commit

---
+++ F405_HAL_SWI2C_MLX90640/Src/main.c
@@ -0,0 +1,235 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include "MLX90640_API.h"
+#include "MLX90640_I2C_Driver.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+#define  Rate2HZ   0x02
+#define  Rate4HZ   0x03
+#define  Rate8HZ   0x04
+#define  Rate16HZ  0x05
+#define  Rate32HZ  0x06
+
+#define  MLX_I2C_ADDR 0x33
+#define	 RefreshRate Rate4HZ 
+#define  TA_SHIFT 8 //Default shift for MLX90640 in open air
+
+static uint16_t eeMLX90640[832];  
+uint16_t frame[834];
+float Ta,tr;
+float emissivity=0.95;
+static float mlx90640To[768];
+int status;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+	MLX90640_I2CInit();
+	
+	MLX90640_SetRefreshRate(MLX_I2C_ADDR, RefreshRate);
+	MLX90640_SetChessMode(MLX_I2C_ADDR);
+	paramsMLX90640 mlx90640;
+  status = MLX90640_DumpEE(MLX_I2C_ADDR, eeMLX90640);
+  if (status != 0) printf("\r\nload system parameters error with code:%d\r\n",status);
+  status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
+  if (status != 0) printf("\r\nParameter extraction failed with error code:%d\r\n",status);
+
+	for(uint8_t i=0;i<3;i++)//Lose the start frame
+	{
+			MLX90640_GetFrameData(MLX_I2C_ADDR, frame);
+			HAL_Delay(500);
+	}
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+			int status = MLX90640_GetFrameData(MLX_I2C_ADDR, frame);
+			if (status < 0)
+			{
+				//printf("GetFrame Error: %d\r\n",status);
+			}
+			float vdd = MLX90640_GetVdd(frame, &mlx90640);
+			float Ta = MLX90640_GetTa(frame, &mlx90640);
+
+			float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
+			//printf("vdd:  %f Tr: %f\r\n",vdd,tr);
+			MLX90640_CalculateTo(frame, &mlx90640, emissivity , tr, mlx90640To);
+
+			printf("\r\n==========================IAMLIUBO MLX90640 WITH STM32 SWI2C EXAMPLE Github:github.com/imliubo==========================\r\n");
+			for(int i = 0; i < 768; i++){
+				if(i%32 == 0 && i != 0){
+					printf("\r\n");
+				}
+				printf("%2.2f ",mlx90640To[i]);
+			}
+			printf("\r\n==========================IAMLIUB0 MLX90640 WITH STM32 SWI2C EXAMPLE Github:github.com/imliubo==========================\r\n");
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # MLX90640-With-STM32
 MLX90640 is a fully calibrated 32x24 pixels thermal IR array sensor.
+
+## MLX90640 with STM32 SWI2C 
+* SDA_PIN:PB7
+* SCL_PIN:PB6
+
+The data will printf on UASRT1 with 115200 buad rate like the blow picture!
+
+![](https://github.com/imliubo/MLX90640-With-STM32/image/MLX90640_STM32_HAL_SWI2C.png)
+
+## MLX90640 with STM32 HWI2C 
+* SDA_PIN:PB7
+* SCL_PIN:PB6
+
+**There are still problems solved**
+
